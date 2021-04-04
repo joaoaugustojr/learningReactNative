bbace8bc3330751e5255ac4d514faa55e19dd5f0
joaoaugust2019 <jhonaugustjunior@gmail.com>
2021-04-03 22:27:39 -0300

    first commit

---
+++ src/styles/global.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1, // Occupation all space of screen
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 12,
+  },
+});
+
+export { globalStyles };
